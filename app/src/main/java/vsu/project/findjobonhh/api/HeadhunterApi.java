47be782f6f480184def7f59a87e7ee8740cea9f3
Vitya535 <kushnerenko.viktor@gmail.com>
2019-05-11 12:47:34 +0300

    Прописал некоторые функции в API.

--- app/src/main/java/vsu/project/findjobonhh/api/HeadhunterApi.java
+++ app/src/main/java/vsu/project/findjobonhh/api/HeadhunterApi.java
@@ -1,11 +1,19 @@
 package vsu.project.findjobonhh.api;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import vsu.project.findjobonhh.models.Vacancy;
+
 public interface HeadhunterApi {
-    String BASE_URL = "";
+    String BASE_URL = "https://api.hh.ru/";
 
     // @POST("/resumes") - Создание резюме (загрузка)
-
-    // проверка резюме на ошибки - делать как-то самим скорее всего придется
 
     // @GET("/vacancies") - Поиск вакансий по выбранным критериям
 
@@ -14,5 +22,25 @@ public interface HeadhunterApi {
 
     // POST /negotiations - отклик на вакансию
 
+    // проверка резюме на ошибки - делать как-то самим скорее всего придется
+
     // Получение контактов разработчиков программы и помощь - сами
+
+    @POST("/resumes")
+    void addResume();
+
+    @GET("/vacancies")
+    Call<List<Vacancy>> getVacancies(@Query("text") String text,
+                                     @Query("search_field") String searchField,
+                                     @Query("salary") Integer salary,
+                                     @Query("period") Integer period);
+
+    @PUT("/employers/{employer_id}/vacancies/hidden/{vacancy_id}")
+    void deleteVacancy(@Path("employer_id") Integer employerId,
+                       @Path("vacancy_id") Integer vacancyId);
+
+    @POST("/negotiations")
+    void respondOnVacancy(@Query("vacancy_id") Integer vacancyId,
+                          @Query("resume_id") Integer resumeId,
+                          @Query("message") String message);
 }
