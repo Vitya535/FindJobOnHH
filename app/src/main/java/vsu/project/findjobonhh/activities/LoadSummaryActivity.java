35c99a85a63eb808c86656b4a1e99f0ab3f73cfd
Vitya535 <kushnerenko.viktor@gmail.com>
2019-05-12 08:34:12 +0300

    Примерно наметил что будет на каждой из активити.

---
+++ app/src/main/java/vsu/project/findjobonhh/activities/LoadSummaryActivity.java
@@ -0,0 +1,16 @@
+package vsu.project.findjobonhh.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+// активити для создания(загрузки) резюме
+// возможно здесь также будет проверка трудового резюме на ошибки (валидация данных при загрузке)
+
+public class LoadSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/vsu/project/findjobonhh/activities/MainPageActivity.java
@@ -0,0 +1,16 @@
+package vsu.project.findjobonhh.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+// Активити главной страницы (с нее же можно залогиниться), с нее можно сделать поиск вакансий
+// С любой из страниц можно сделать logout (выход из аккаунта), загрузку (создание) резюме
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
